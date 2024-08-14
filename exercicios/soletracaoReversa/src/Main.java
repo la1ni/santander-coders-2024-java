4286ea6abb58e019e1cb2b512f9cb497858e291c
la1ni <marilainarasilva@hotmail.com>
2024-08-14 10:47:35 -0300

    Adicionado exercício Soletração Reversa

---
+++ exercicios/soletracaoReversa/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(Arrays.toString(soletrandoInvertidoStr("amor")));
+    }
+    public static String[] soletrandoInvertidoStr(String input) {
+        String[] invertida = new String[input.length()];
+        int contadorPositivo = 0;
+        for (int i = input.length()-1; i >= 0; i--){
+
+            invertida[i] = String.valueOf(input.charAt(contadorPositivo));
+
+            contadorPositivo++;
+
+        }
+
+        return invertida;
+    }
+}
