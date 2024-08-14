26272f4c197462e04ce9da30cc35b6c629aa38f3
la1ni <marilainarasilva@hotmail.com>
2024-08-14 10:36:36 -0300

    Adicionado exercício Primeiro Caractere Único

--- exercicios/primeiroCaractereUnico/src/Main.java
+++ exercicios/primeiroCaractereUnico/src/Main.java
@@ -2,14 +2,18 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println(primeiroCaractereUnico("amor"));
+    }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static int primeiroCaractereUnico(String input) {
+        char caractereAnalisado;
+        for (int i = 0; i < input.length(); i++){
+            caractereAnalisado = input.charAt(i);
+            if (i == input.lastIndexOf(caractereAnalisado)) {
+                return i;
+            }
         }
+
+        return -1;
     }
 }
